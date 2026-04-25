b79fb11cf8b019f9a27c5f6bbeb87952cf8bc25c
BaekCheon Seong <bowow0310@gmail.com>
2026-04-25 12:08:55 +0900

    Fix virtual environment activation path in run_stereo_calibration.bat to reference parent folder

--- run_stereo_calibration.bat
+++ run_stereo_calibration.bat
@@ -2,23 +2,12 @@
 setlocal enabledelayedexpansion
 cd /d "%~dp0"
 
-<<<<<<< HEAD
-if not exist ".\.venv39\Scripts\activate.bat" (
-    echo Error: .venv39 not found in this folder. Check venv path.
-    pause
-    exit /b 1
-)
-call ".\.venv39\Scripts\activate.bat"
-
-cd "%~dp0stereo-calibration"
-=======
 if not exist "%~dp0..\.venv39\Scripts\activate.bat" (
     echo Error: .venv39 not found in parent folder. Check venv path.
     pause
     exit /b 1
 )
 call "%~dp0..\.venv39\Scripts\activate.bat"
->>>>>>> master
 echo Running: python -m calib_v3.main %*
 
 python -m calib_v3.main %*
